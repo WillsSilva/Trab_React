b9e5000da0cda65b8082d024d0af3d91592fd5db
andrwi <andrwi@2020spaces.com>
2023-10-11 22:15:09 -0300

    Adicionado front e api get da api para deletar

--- my-api-hook/src/App.jsx
+++ my-api-hook/src/App.jsx
@@ -1,25 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFindBooks } from './hooks/useFindBooks';
+import './App2.css';
+import axios from 'axios';
+
 function App() {
-  // use seu próprio hook para carregar os dados da API
-  const { data, loading, error } = useFindBooks()
-  if (loading) return <div><img id='Load' src="https://media.tenor.com/6gHLhmwO87sAAAAj/gg.gif" alt="" /></div>
-  if (error) return <div>{error}</div>
+  const { data, loading, error } = useFindBooks();
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  if (loading) return <LoadingSpinner />;
+  if (error) return <ErrorDisplay error={error} />;
+
+  const openBookCard = (book) => {
+    setSelectedBook(book);
+  };
+
   return (
-    <div>
-      <h3>Consulta à API</h3>
-      <ul>
-        {
-          data &&
+    <div className="app-container">
+      <h1 className="app-title">Lista de Livros</h1>
+      <button id="Add">Adicionar</button>
+      <ul className="book-list">
+        {data &&
           data.length > 0 &&
-          data.map(item => (
-            <li key={item.id}>
-              {item.title}: {item.description}
-            </li>
-          ))
-        }
+          data.map((item) => (
+            <BookItem
+              key={item.id}
+              title={item.title}
+              description={item.description}
+              onEdit={() => openBookCard(item)}
+              onDelete={() => onDeleteBook(item.id)} // Correção aqui
+            />
+          ))}
       </ul>
+      {selectedBook && (
+        <BookCard
+          title={selectedBook.title}
+          description={selectedBook.description}
+          onClose={() => setSelectedBook(null)}
+        />
+      )}
     </div>
   );
 }
+
+function LoadingSpinner() {
+  return (
+    <div className="loading-container">
+      <img src="https://media.tenor.com/6gHLhmwO87sAAAAj/gg.gif" alt="Loading" />
+    </div>
+  );
+}
+
+function ErrorDisplay({ error }) {
+  return <div className="error-container">{error}</div>;
+}
+
+function BookItem({ title, description, onDelete, onEdit }) {
+  return (
+    <li className="book-item">
+      <strong className="book-title">{title}:</strong> {description}
+      <button onClick={onEdit} id="Editar">Editar</button>
+      <button onClick={onDelete} id="Excluir">Excluir</button>
+    </li>
+  );
+}
+
+function BookCard({ title, description, onClose }) {
+  return (
+    <div className="book-card">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <button onClick={onClose}>Fechar</button>
+    </div>
+  );
+}
+
+function onDeleteBook(id) {
+  // Implemente a lógica de exclusão do livro aqui
+  axios.delete(`https://fakerestapi.azurewebsites.net/api/v1/Books/${id}`)
+    .then(response => {
+      // Faça algo com a resposta, se necessário
+      console.log(response.status);
+    })
+    .catch(error => {
+      // Trate os erros, se houver
+      console.error(error);
+    });
+}
+
+
 export default App;
